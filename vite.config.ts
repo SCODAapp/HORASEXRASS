e11ede01f58b454d72e963e11b4ba6d0cf78e96b
SCODAapp <scodaapp@gmail.com>
2026-01-10 19:19:12 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HORASEXRASS/',
+  base: '/HEXTRAS/',
   server: {
     port: 5173,
     host: true
