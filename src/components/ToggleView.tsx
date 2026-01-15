6e4ba273fd6800e2cea20c6b2b2dfe3bff191275
SCODAapp <scodaapp@gmail.com>
2026-01-15 20:16:57 +0100

    Delete src/components/ToggleView.tsx

--- src/components/ToggleView.tsx
+++
@@ -1,7 +0,0 @@
-interface ToggleViewProps {
-  onToggleView: () => void;
-}
-
-export default function ToggleView({ onToggleView }: ToggleViewProps) {
-  return <button onClick={onToggleView}>Cambiar vista</button>;
-}
