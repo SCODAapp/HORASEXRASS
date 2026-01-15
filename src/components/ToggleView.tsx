0c122026804d9e6934e8061e9321814660bd3576
SCODAapp <scodaapp@gmail.com>
2026-01-15 19:54:34 +0100

    Add ToggleView component for view toggling

---
+++ src/components/ToggleView.tsx
@@ -0,0 +1,7 @@
+interface ToggleViewProps {
+  onToggleView: () => void;
+}
+
+export default function ToggleView({ onToggleView }: ToggleViewProps) {
+  return <button onClick={onToggleView}>Cambiar vista</button>;
+}
