7a88fa1b24dca2d664ed02541021396b218cf636
SCODAapp <scodaapp@gmail.com>
2026-02-02 13:52:39 +0100

    Updated tsconfig.node.tsbuildinfo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: '/horasextras/',
+    base: '/HORASEXRASS/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
